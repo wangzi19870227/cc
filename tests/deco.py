46dc55b140115f70a863c9c3360474f9a556e4ea
赵海川 <zhaohaichuan@le.com>
2017-07-19 17:24:34 +0800

    deco support args

--- tests/deco.py
+++ tests/deco.py
@@ -1,22 +1,40 @@
-def deco(func):
+import functools
 
+
+def log(func):
+    # should run this, or outside myfunc.__name__ will return '_deco'
+    @functools.wraps(func)
     def _deco(*args, **kwargs):
-
         print("before %s called." % func.__name__)
-
         ret = func(*args, **kwargs)
-
         print("  after %s called. result: %s" % (func.__name__, ret))
-
         return ret
-
     return _deco
 
-@deco
+def log_with_args(text):
+    def _wrap(func):
+        # should run this, or outside myfunc.__name__ will return '_deco'
+        @functools.wraps(func) 
+        def _deco(*args, **kwargs):
+            print("before %s %s called." % (text, func.__name__))
+            ret = func(*args, **kwargs)
+            print("  after %s %s called. result: %s" % (text, func.__name__, ret))
+            return ret
+        return _deco
+    return _wrap
+
+@log
 def myfunc(a, b):
-
     print(" myfunc(%s,%s) called." % (a, b))
+    return a+b
 
+@log_with_args('excute')
+def myfunc2(a, b):
+    print(" myfunc2(%s,%s) called." % (a, b))
     return a+b
 
 myfunc(1, 2)
+print myfunc.__name__
+
+myfunc2(1, 2)
+print myfunc2.__name__
