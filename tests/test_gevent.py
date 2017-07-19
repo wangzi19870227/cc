9cedf6c8d2f37b8854a914ea05a807b81a478292
赵海川 <zhaohaichuan@le.com>
2017-07-19 18:07:32 +0800

    update deco.py gevent.py

--- tests/test_gevent.py
+++ tests/test_gevent.py
@@ -1,4 +1,3 @@
-import gevent
 from gevent import monkey; monkey.patch_socket()
 import gevent
 
@@ -6,13 +5,15 @@ import gevent
 def f(n):
     for i in range(n):
         print gevent.getcurrent(), i
+        #raise Exception('xxx')
+        gevent.sleep(0)
 
 g1 = gevent.spawn(f, 5)
 g2 = gevent.spawn(f, 5)
 g3 = gevent.spawn(f, 5)
+print 'gevents spawn done.'
+
 g1.join()
 g2.join()
 g3.join()
-
-
-
+print'gevents join done.'
